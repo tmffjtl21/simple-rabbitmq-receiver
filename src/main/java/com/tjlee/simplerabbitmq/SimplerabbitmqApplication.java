60abbfae9eb22e7ab698064159034b1f7a124bf6
LeeTaejin <tmffjtl21@osstem.com>
2019-08-27 16:32:52 +0900

    initial commit

---
+++ src/main/java/com/tjlee/simplerabbitmq/SimplerabbitmqApplication.java
@@ -0,0 +1,33 @@
+package com.tjlee.simplerabbitmq;
+
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class SimplerabbitmqApplication {
+
+    static final String queueName = "spring-boot";
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimplerabbitmqApplication.class, args);
+    }
+
+    @Bean
+    MessageListenerAdapter listenerAdapter(Receiver receiver) {
+        return new MessageListenerAdapter(receiver, "receiveMessage");
+    }
+
+    @Bean
+    SimpleMessageListenerContainer container(ConnectionFactory connectionFactory,
+                                             MessageListenerAdapter listenerAdapter) {
+        SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
+        container.setConnectionFactory(connectionFactory);
+        container.setQueueNames(queueName);
+        container.setMessageListener(listenerAdapter);
+        return container;
+    }
+}
